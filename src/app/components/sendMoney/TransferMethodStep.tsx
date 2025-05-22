4f3b8eafe2ed6d364220c3a43e8396eebcebbaca
jasper tech <curdyj78@gmail.com>
2025-05-22 09:46:16 +0000

    updates

--- src/app/components/sendMoney/TransferMethodStep.tsx
+++ src/app/components/sendMoney/TransferMethodStep.tsx
@@ -15,15 +15,6 @@ export const TransferMethodStep = ({
       How would you like to send money?
     </h2>
     <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-      {/* <TransferMethodOption
-        method="email"
-        title="Send to Email"
-        description="Send money using recipient's email address"
-        icon={<User className="w-8 h-8 text-blue-600 mb-4" />}
-        isSelected={transferMethod === "email"}
-        onClick={() => setTransferMethod("email")}
-      /> */}
-
       <TransferMethodOption
         method="phone"
         title="Mobile Money"
